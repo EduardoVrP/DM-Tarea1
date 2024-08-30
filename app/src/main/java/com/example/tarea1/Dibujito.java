8df2d041da8ddaaa8c97330d76f78a58fb4669fb
EduardoVrP <eduardovar1090@gmail.com>
2024-08-29 20:28:22 -0600

    Parece que ya jala

---
+++ app/src/main/java/com/example/tarea1/Dibujito.java
@@ -0,0 +1,13 @@
+package com.example.tarea1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Dibujito extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
